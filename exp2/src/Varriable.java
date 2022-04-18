364ba4ba7bc54c050572b5ee26e609075a2f0425
JunDev1 <86519269+JunDev1@users.noreply.github.com>
2022-04-18 19:57:26 +0300

    Delete exp2 directory

--- exp2/src/Varriable.java
+++
@@ -1,8 +0,0 @@
-public class Varriable {
-        public static void main(String[]args) {
-            double a = 3.0;
-            double b = 4.0;
-            double c = Math.sqrt(a * a + b * b);
-            System.out.println("c = " + c);
-        }
-    }
